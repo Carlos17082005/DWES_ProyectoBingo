81a3a45e9fad1f0057be5ddcd1ad42adeb747a35
unknown <carlosggarcia.guzman@gmail.com>
2025-10-08 10:16:02 +0200

    Funciona Juegobingo.php y bingo.php en functions.php y añade index.php

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+    function imprimirCarton()  { //imprme el carton en forma de tabla
+        echo "<table>";
+        $a=carta();
+        foreach ($a as $fila) {
+            echo "<tr>"; 
+            foreach ($fila as $valor) {
+                echo "<td>".$valor."</td>"; 
+            }
+            echo "</tr>"; 
+        }
+        echo "</table>";
+    }
+    
+    $bolas=array(); //array que guarda todas las bolas que van saliendo
+    function sacarBola()  { //saca un numero aleatorio del 1 al 60
+        $bola=(int)rand(1,60);
+        echo '<p style="color: red;">'.$bola.'</p>';
+        return $bola;
+    }
+
+    function carta()
+    {
+        $carta=array();
+        $numerosUsados = []; //guardar todos los numeros ya usados para evitar numeros repetidos
+
+        for($i=0;$i<3;$i++) //3 filas
+        {
+            $fila = array_fill(0,7,null); // se crea una fila con 7 columnas, todas inicialmente null
+            
+            // Elegir 5 posiciones aleatorios diferentes dentro de la fila
+            $posiciones = [];
+            while (count($posiciones) < 5){
+                $pos = rand(0,6); //indice entre 0 y 6
+                if (!in_array($pos, $posiciones))
+                    $posiciones[] = $pos;
+            }
+
+            // Asignar numeros aleatorios en esas posiciones
+            foreach ($posiciones as $pos){
+                do{
+                    $num = rand(1,60);
+                } while (in_array($num, $numerosUsados)); // Para que no se repita
+
+                $fila[$pos] = $num;
+                $numerosUsados[] = $num;
+            }
+
+            // Añadir fila a la carta
+            $carta[] = $fila;
+
+        }
+        return $carta;
+    }
+    
+    function cartasJugador()
+    {
+        $cartas = array();
+        for($i=0; $i < 3;$i++)
+        {
+            $cartas[] = carta();
+        }
+        return $cartas;
+    }
+
+    $jugador1 = cartasJugador();
+    $jugador2 = cartasJugador();
+    $jugador3 = cartasJugador();
+    $jugador4 = cartasJugador();
+
+
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bingo</title>
+    <style>
+        h1  {
+            text-align: center;
+            font-size: 48px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+        .jugador  {
+            width: 24%;
+            margin: 1px;
+            border: 1px red solid;
+            float: left;
+        }
+    </style>
+</head>
+<body>
+    <h1>Bingo</h1>
+    <div class="jugador" id="j1">
+        <?php?>
+    </div>
+    <div class="jugador" id="j2"><p>hola</p></div>
+    <div class="jugador" id="j3"><p>hola</p></div>
+    <div class="jugador" id="j4"><p>hola</p></div>
+
+
+
+</body>
+</html>
